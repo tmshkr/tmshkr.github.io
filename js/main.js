da65f78a444333643c690a3393ff0cf6e0fdd6ab
tmshkr <tmshkr@users.noreply.github.com>
2018-09-23 22:35:29 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var media = window.matchMedia('(max-width: 600px)');
 function autoHideNavbar() {
 	let currentScrollPos = window.pageYOffset;
 	//scrolling up
-	if (!visible && prevScrollPos - currentScrollPos > 40 ||
+	if (!visible && prevScrollPos - currentScrollPos > 30 ||
 		!visible && currentScrollPos < pageTop) {
 		(!window.requestAnimationFrame) ?
 		navbar.style.transform = 'translateY(0)':
@@ -23,7 +23,7 @@ function autoHideNavbar() {
 		visible = true;
 	}
 	//scrolling down
-	else if (visible && currentScrollPos - prevScrollPos > 20 &&
+	else if (visible && currentScrollPos - prevScrollPos > 10 &&
 		currentScrollPos > pageTop) {
 		(!window.requestAnimationFrame) ?
 		navbar.style.transform = 'translateY(-100%)':
